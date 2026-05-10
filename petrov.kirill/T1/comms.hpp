1abd5ff16cc42ee3dfc258f883ca59be17ea7a14
Kirill <kirillpetrov56333@gmail.com>
2026-05-10 14:45:30 +0300

    mem

--- petrov.kirill/T1/comms.hpp
+++ petrov.kirill/T1/comms.hpp
@@ -1,8 +1,18 @@
 #ifndef COMMS_HPP
 #define COMMS_HPP
+#include <string>
+#include <vector>
 
 namespace petrov
 {
+  struct mem
+  {
+    mem(str::string s);
+    std::string name;
+    std::string lines;
+    std::vector<std::weak_ptr<mem>> txt;
+  };
+  
 }
 
 #endif
