c6c3e267e895d2ec95f92fbe43082eed07041df3
Kirill <kirillpetrov56333@gmail.com>
2026-05-10 15:02:04 +0300

    full comms.hpp

--- petrov.kirill/T1/comms.hpp
+++ petrov.kirill/T1/comms.hpp
@@ -17,7 +17,15 @@ namespace petrov
     public:
       void reg_comm(std::iostream& in, std::ostream& ou);
     private:
-      std::unordered_map
+      std::unordered_map <std::string, std::shared_ptr<mem>> storage;
+      bool note(std::istream& in);
+      bool line(std::istream& in);
+      bool show(std::istream& in, std::ostream& ou);
+      bool drop(std::istream& in);
+      bool link(std::istream& in);
+      bool mind(std::istream& in, std::ostream& ou);
+      bool expiried(std::istream& in, std::ostream& ou);
+      bool refresh(std::istream& in);
   };
 }
 
