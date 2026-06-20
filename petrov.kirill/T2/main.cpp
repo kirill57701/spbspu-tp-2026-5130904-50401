d92f07802a9d7fcbfc7238924206bab012d57c30
Kirill <kirillpetrov56333@gmail.com>
2026-06-20 15:58:01 +0300

    new funcs

--- petrov.kirill/T2/main.cpp
+++ petrov.kirill/T2/main.cpp
@@ -30,5 +30,33 @@ namespace petrov
   public:
     explicit Format(std::basic_ios<char>& stream);
     ~Format();
+
+  private:
+    std::basic_ios<char>& stream_;
+    char fill_;
+    std::streamsize prec_;
+    std::basic_ios<char>::fmtflags fl_;
   };
+
+  std::istream& operator>>(std::istream& in, ExpChar&& d);
+  std::istream& operator>>(std::istream& in, ReadOct&& d);
+  std::istream& operator>>(std::istream& in, ReadHex&& d);
+  std::istream& operator>>(std::istream& in, ReadStr&& d);
+  std::istream& operator>>(std::istream& in, DataStruct& d);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src);
+
+  bool compareData(const DataStruct& lhs, const DataStruct& rhs);
+
+  FormatGuard::FormatGuard(std::basic_ios<char>& stream):
+      stream_(stream),
+      fill_(stream.fill()),
+      precision_(stream.prec()),
+      fl_(stream.flags()) {
+  }
+
+  FormatGuard::~FormatGuard() {
+    stream_.fill(fill_);
+    stream_.precision(prec_);
+    stream_.flags(fl_);
+  }
 }
