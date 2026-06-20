cd0a79969c39c968db9bbbc719ed30f10e3f2e91
Kirill <kirillpetrov56333@gmail.com>
2026-06-20 16:28:37 +0300

    funcs

--- petrov.kirill/T2/main.cpp
+++ petrov.kirill/T2/main.cpp
@@ -107,4 +107,78 @@ namespace petrov
     in >> std::hex >> dest.ref >> std::dec;
     return in;
   }
+  std::istream& operator>>(std::istream& in, ReadString&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    return std::getline(in >> ExpectChar{'"'}, dest.ref, '"');
+  }
+
+  std::istream& operator>>(std::istream& in, DataStruct& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+
+    DataStruct temp{0, 0, ""};
+    in >> ExpectChar{'('} >> ExpectChar{':'};
+
+    for (int i = 0; i < 3; ++i) {
+      std::string key;
+      std::getline(in, key, ' ');
+
+      if (key == "key1") {
+        in >> ReadOct{temp.key1};
+      } else if (key == "key2") {
+        in >> ReadHex{temp.key2};
+      } else if (key == "key3") {
+        in >> ReadString{temp.key3};
+      } else {
+        in.setstate(std::ios::failbit);
+      }
+
+      if (i < 2) {
+        in >> ExpectChar{':'};
+      }
+    }
+
+    in >> ExpectChar{':'} >> ExpectChar{')'};
+
+    if (in) {
+      dest = temp;
+    }
+
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src) {
+    std::ostream::sentry sentry(out);
+    if (!sentry) {
+      return out;
+    }
+
+    FormatGuard guard(out);
+
+    out << "(:key1 ";
+    if (src.key1 == 0) {
+      out << "0";
+    } else {
+      out << "0" << std::oct << src.key1;
+    }
+
+    out << ":key2 0x" << std::hex << std::uppercase << src.key2
+        << ":key3 \"" << src.key3 << "\":)";
+
+    return out;
+  }
+  bool compareData(const DataStruct& lhs, const DataStruct& rhs) {
+    if (lhs.key1 != rhs.key1) {
+      return lhs.key1 < rhs.key1;
+    }
+    if (lhs.key2 != rhs.key2) {
+      return lhs.key2 < rhs.key2;
+    }
+    return lhs.key3.length() < rhs.key3.length();
+  }
 }
