2de491f0bcd7a4281ebda6fe3687a12fe030ec23
Kirill <kirillpetrov56333@gmail.com>
2026-06-20 12:55:58 +0300

    add datastruct

---
+++ petrov.kirill/DataStruct.hpp
@@ -0,0 +1,14 @@
+#ifndef PETROV_DATA_STRUCT_HPP
+#define PETROV_DATA_STRUCT_HPP
+#include <string>
+
+namespace petrov
+{
+  struct DataStruct
+  {
+    size_t k1, k2;
+    std::string k3;
+  }
+}
+
+#endif
